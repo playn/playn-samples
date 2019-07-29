471108d9d7bc4e61566c39e8bc7a73f25eed8e1f
Michael Bayne <mdb@samskivert.com>
2019-07-29 09:55:10 -0700

    Remove flurry example.
    It hasn't been updated to 2.x and is probably not worth the trouble.

--- flurry/core/src/main/java/playn/flurry/core/Flurry.java
+++
@@ -1,8 +0,0 @@
-package playn.flurry.core;
-
-public interface Flurry {
-
-  /** Logs a named event.
-   * @param args zero or more (string) key / (string or int) value pairs. */
-  void logEvent (String eventName, Object... args);
-}
